3c663697dd3b5c3b16785ae072540c088fa010ef
Gabriel D. Sulé <gabrielsule@gmail.com>
2020-08-10 08:10:10 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow('frame', frame)
+   
+    k = cv2.waitKey(1) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
